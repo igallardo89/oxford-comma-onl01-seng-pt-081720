ce21f26ace06c28726d2692f4c8ddefaf8bb7936
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-25 18:44:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join("and ")
     array.join(", ")
-  elsif array.last.insert(0," and ")
+  elsif array.last.insert(0,"and ")
       array.join(", ")
   else 
   return
