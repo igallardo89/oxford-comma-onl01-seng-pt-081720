053f16c51c59724c5a070c3ad99314afdc3c4232
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-25 18:48:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
+  array.join
+  
   if array.length == 2
     return array.join(" and ")
     array.join(", ")
   elsif array.last.insert(0,"and ")
      array.join(", ")
-  else 
-    return array.join
+  else
+    
   end
 end
