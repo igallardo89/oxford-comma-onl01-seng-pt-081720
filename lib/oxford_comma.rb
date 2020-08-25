beb08bd30220a4400f3d4a4a7b66e6916cc0a7d0
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-25 18:43:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
     array.join(", ")
-  elsif array.last.insert(0,", and ")
-      array.join(", ")
+  elsif array.last.insert(0," and ")
+      array.join(" ")
   else 
   return
   
